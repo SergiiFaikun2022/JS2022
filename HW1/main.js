ce91d8611b319b5fa0b6f3e163d9c33d52de819b
SergiiFaikun2022 <testerlviv2022>
2022-07-22 14:37:18 +0300

    HW1 first addition

---
+++ HW1/main.js
@@ -0,0 +1,135 @@
+
+// part1
+
+// let num1 = 'hello';
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+//
+// let num2 = 'owu';
+// console.log(num2);
+// alert(num2);
+// document.write(num1);
+//
+// let num3 = 'com';
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+//
+//
+// let num4 = 'ua';
+// console.log(num4);
+// alert(num4);
+// document.write(num4)
+//
+// let num5 = 1;
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+//
+// num6 = 10;
+// console.log(num6);
+// alert(num6);
+// document.write(num6);
+//
+// num7 = -999;
+// console.log(num7);
+// alert(num7);
+// document.write(num7);
+//
+// num8 = 123;
+// console.log(num8);
+// alert(num8);
+// document.write(num8);
+//
+// num9 = 3.14;
+// console.log(num9);
+// alert(num9);
+// document.write(num9);
+//
+// num10 = 2.7;
+// console.log(num10);
+// alert(num10);
+// document.write(num10);
+//
+// num11 = 16;
+// console.log(num11);
+// alert(num11);
+// document.write(num11);
+//
+// num12 = true;
+// console.log(num12);
+// alert(num12);
+// document.write(num12);
+//
+//
+// num13 = false;
+// console.log(num13);
+// alert(num13);
+// document.write(num13);
+
+
+
+
+
+// part 2
+
+
+// let firstName ='Sergii';
+// let middleName = 'Vasilovich';
+// let lastName = 'Faikun';
+//
+// let space = ' ';
+//
+// let person = (firstName + space + middleName + space + lastName);
+// console.log(person);
+
+
+// part 3
+
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+
+
+// part 4
+
+
+let inputFirstName = prompt('inputFirstName');
+
+let inputMiddleName = prompt('inputFirstName');
+let inputLastName = prompt('inputFirstName');
+let space = ' ';
+
+let person = (inputFirstName + space + inputMiddleName + space + inputLastName);
+
+console.log(person);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
