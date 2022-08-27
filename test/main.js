c3ab0a8b2bf30a3f4e4ba458f12b7f0e0362de03
SergiiFaikun2022 <testerlviv2022>
2022-08-27 12:21:30 +0300

    mini-project

--- test/main.js
+++ test/main.js
@@ -127,3 +127,13 @@
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
 
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+coursesAndDurationArray.forEach()
